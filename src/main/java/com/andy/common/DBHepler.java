7b8034c6590dba316949b228e879c697c53cb978
andy <jiahuihb@126.com>
2017-03-01 14:55:41 +0800

    foot map

---
+++ src/main/java/com/andy/common/DBHepler.java
@@ -0,0 +1,12 @@
+package com.andy.common;
+
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBHepler {
+	public Connection getDBConnection(){
+		List list = new ArrayList();
+		return null;
+	}
+}
